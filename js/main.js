c35df28df11fdfed6a25f040d91a5a346a0c671c
Adriel Z�rate <adriel_zarate@hotmail.com>
2013-12-10 01:23:10 -0300

    nuevo plugin de pintura y funcionalidad ctrl+z

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+/*
+script viejo de pincel
+
 (function() {
 	var canvas = document.querySelector('#paint');
 	var ctx = canvas.getContext('2d');
@@ -133,3 +136,30 @@
 		ctx.stroke();	
 	};
 }());
+*/
+
+var anchoVentana = $(window).width();
+var altoVentana = $(window).height();
+
+$('#sketch').wScratchPad({
+    width           : anchoVentana,                  // set width - best to match image width
+    height          : altoVentana,                  // set height - best to match image height
+    image           : null,  // set image path
+    image2          : null,                 // set overlay image path - if set color is not used
+    color           : '#fff',            // set scratch color - if image2 is not set uses color
+    overlay         : 'none',               // set the type of overlay effect 'none', 'lighter' - only used with color
+    size            : 50,                   // set size of scratcher
+    realtimePercent : false,                // Update scratch percent only on the mouseup/touchend (for better performances on mobile device)
+    scratchDown     : null,                 // scratchDown callback
+    scratchUp       : null,                 // scratchUp callback
+    scratchMove     : null,                 // scratcMove callback
+    cursor          : 'img/cursor.png'                  // Set path to custom cursor
+});
+
+
+$(window).keypress(function(event) {
+    if (!(event.which == 115 && event.ctrlKey) && !(event.which == 122)) return true;
+    $('#sketch').wScratchPad('reset');
+    event.preventDefault();
+    return false;
+});
